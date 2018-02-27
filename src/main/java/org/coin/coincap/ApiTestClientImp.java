76304a055226cb24b73b423d8ce8ba8009738517
Dominik Baranec <dominik.baranec@scr.sk>
2018-02-27 20:04:33 +0100

    [*] implemented Component, used yml value

--- src/main/java/org/coin/coincap/ApiTestClientImp.java
+++ src/main/java/org/coin/coincap/ApiTestClientImp.java
@@ -2,13 +2,19 @@ package org.coin.coincap;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 
+@Component
 public class ApiTestClientImp {
 
-    private static final String URI = "https://jsonplaceholder.typicode.com/";
+    //private String URI = "https://jsonplaceholder.typicode.com/";
+    @Value("${apiTest.url}")
+    private String URI;
 
     public List<BookResponse> getBooks() throws Exception {
         ApiTestClient apiTestClient = Feign.builder().encoder(new JacksonEncoder()).decoder(new JacksonDecoder())
--- src/main/java/org/coin/coincap/CoincapApplication.java
+++ src/main/java/org/coin/coincap/CoincapApplication.java
@@ -3,23 +3,31 @@ package org.coin.coincap;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@ComponentScan("org.coin.coincap")
 public class CoincapApplication {
+
+    @Autowired ApiTestClientImp apiTestClientImp;
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World! ";
     }
 
     @RequestMapping("/api")
     BookResponse api() throws Exception{
-        ApiTestClientImp apiTestClientImp = new ApiTestClientImp();
         List<BookResponse> bookResponseList = apiTestClientImp.getBooks();
 
         return bookResponseList.get(0);
