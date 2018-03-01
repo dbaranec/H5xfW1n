0b31a42dd11026091279b7b231bb00364a4f7cbc
Dominik Baranec <dominik.baranec@scr.sk>
2018-03-01 21:17:12 +0100

    [*]clean up code

--- src/main/java/org/coin/coincap/CoincapApplication.java
+++ src/main/java/org/coin/coincap/CoincapApplication.java
@@ -4,13 +4,9 @@ package org.coin.coincap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
